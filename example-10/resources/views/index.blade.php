d406237b2f551b76376791565c6caa26bd62b936
Gbearup <bearapo945@gmail.com>
2025-02-18 15:15:26 +0800

    new

---
+++ example-10/resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <a href="./f1">1F</a>
+    </p>
+
+    <p>
+        <a href="./f2">2F</a>
+    </p>
+
+    <p>
+        <a href="./f3">3F</a>
+    </p>
+
+
+</body>
+</html>
